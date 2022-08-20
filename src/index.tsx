be6910b2ccf779dc78d436bb0d9ca3d8974919bc
Omar Khan <khancodegt@gmail.com>
2022-08-20 12:36:18 -0400

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 
-ReactDOM.render(
-   <App compiler="TypeScript" framework="React" />,
-   document.getElementById('root'),
-);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App compiler="TypeScript" framework="React" />);
